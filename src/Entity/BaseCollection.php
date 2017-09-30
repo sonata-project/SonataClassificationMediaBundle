9a7569a399085949391a9fc14883a4d65360db24
core23 <mail@core23.de>
2017-09-30 09:49:26 +0200

    Added missing base entities

---
+++ src/Entity/BaseCollection.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+ * This file is part of the Sonata Project package.
+ *
+ * (c) Thomas Rabaix <thomas.rabaix@sonata-project.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Sonata\ClassificationMediaBundle\Entity;
+
+use Sonata\ClassificationMediaBundle\Model\Collection as ModelCollection;
+
+abstract class BaseCollection extends ModelCollection
+{
+}
